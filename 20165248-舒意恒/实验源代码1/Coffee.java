7b3885b31525f762e0b980d6762f62d7997925b6
Yiheng Shu <lzw429@qq.com>
2017-04-24 19:09:24 +0800

    delete the zip

--- 20165248-舒意恒/实验源代码1/Coffee.java
+++ 20165248-舒意恒/实验源代码1/Coffee.java
@@ -1,1 +1,26 @@
-public 
+public class Coffee extends Product
+{
+    //成员变量
+    private String origin;
+    private String roast;
+    private String favor;
+    private String aroma;
+    private String acidity;
+    private String body;
+
+    //成员方法
+    //构造方法
+    public Coffee(String initialCode, String initialDescription, double initialPrice, String initialOrigin, String initialRoast, String initialFlavor,String initialAroma, String initialAcidity, String initialBody)
+    {
+        code=initialCode;
+        description=initialDescription;
+        price=initialPrice;
+        origin=initialOrigin;
+        roast=initialRoast;
+        flavor=initialFlavor;
+        aroma=initialAroma;
+        acidity=initialAcidity;
+        body=initialBody;
+    }
+
+}
---
+++ 20165248-舒意恒/实验源代码1/Product.java
@@ -0,0 +1,36 @@
+public class Product
+{
+    //成员变量
+    private String code;
+    private String description;
+    private double price;
+
+    //成员方法
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public double getPrice()
+    {
+        return price;
+    }
+
+    public String getCode()
+    {
+        return getCode();
+    }
+
+    public boolean equals(object:Object)
+    {
+
+    }
+
+    public String toString()
+    {
+        
+    }
+
+    //构造方法
+    public Product(String initialCode, String initialDescription, double initialPrice)
+}
